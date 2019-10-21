60e70d222a069ee1d8074459e0374540ba8a798b
Aaron Cherniavsky <aaron.b.cherniavsky@gmail.com>
2019-10-21 17:18:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,7 @@ numbers= [1,2,3,]
 
 def square_array (numbers)
   squared = []
-  numbers.each { |element| squared << element ** 2 } squared
+  numbers.each { |element| squared << element ** 2 } 
+  squared
 
 end
